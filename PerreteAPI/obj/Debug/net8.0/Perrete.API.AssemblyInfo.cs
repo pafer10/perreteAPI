f3b55151e1e28e09a86931f87ad0602807881695
pafer10 <pafernandez10@gmail.com>
2025-11-11 13:33:56 +0100

    delete connectionStrings

--- PerreteAPI/obj/Debug/net8.0/Perrete.API.AssemblyInfo.cs
+++ PerreteAPI/obj/Debug/net8.0/Perrete.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Perrete.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa9be0b79d5467cfa073446390fe81f0a376df84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Perrete.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Perrete.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
